1d9f628c5bf32da0522bb90c08f6e451be2f52ae
Andrea Casino <andreacasino@andreas-mbp.home>
2019-10-23 23:49:05 +0100

    Added locations and skills endpoints

--- src/lib/joiSchemas.js
+++ src/lib/joiSchemas.js
@@ -1,10 +1,10 @@
 const Joi = require('@hapi/joi');
 
-const email = Joi.string().email().min(3).max(100);
+const email = Joi.string().email().min(3).max(50);
 
 const shortString = Joi.string().min(3).max(30);
 
-const mediumString = Joi.string().min(3).max(500);
+const mediumString = Joi.string().max(500);
 
 const longString = Joi.string().min(3).max(5000);
 
@@ -18,11 +18,11 @@ const createUserSchema = Joi.object({
   email: email.required(),
   username: shortString.required(),
   password: shortString.required(),
-  firstName: shortString,
-  lastName: shortString,
-  bio: mediumString,
-  locationId: locationIdSchema,
-  skillIds: skillIdsSchema,
+  firstName: shortString.optional().allow('').allow(null),
+  lastName: shortString.optional().allow('').allow(null),
+  bio: mediumString.optional().allow('').allow(null),
+  locationId: locationIdSchema.optional().allow('').allow(null),
+  skillIds: skillIdsSchema.optional().allow('').allow(null),
 });
 
 const loginUserSchema = Joi.object({
@@ -34,10 +34,10 @@ const createProjectSchema = Joi.object({
   name: mediumString.required(),
   description: longString.required(),
   skillsNeeded: skillIdsSchema.required(),
-  locationId: locationIdSchema,
-  inspiredBy: longString,
-  assets: longString,
-  contact: Joi.object(),
+  locationId: locationIdSchema.optional().allow('').allow(null),
+  inspiredBy: longString.optional().allow('').allow(null),
+  assets: longString.optional().allow('').allow(null),
+  contact: Joi.object().optional().allow('').allow(null),
 });
 
 const addFavoriteSchema = Joi.object({
